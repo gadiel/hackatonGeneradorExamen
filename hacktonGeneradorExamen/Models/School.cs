b5a157ba44124e22ba62cd9a1835a6595d2ab3e8
Alex Fernandez <afernandez@unitec.edu>
2014-03-01 11:28:01 -0600

    definition of the entities

---
+++ hacktonGeneradorExamen/Models/School.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace hacktonGeneradorExamen.Models
+{
+    public class School
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ hacktonGeneradorExamen/Models/enums.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace hacktonGeneradorExamen.Models
+{
+
+    public enum QuestionFormat
+    {
+        TrueFalse,
+        MultiChoice,
+        MultipleResponse
+    }
+
+    public enum QuestionType
+    {
+        Image,
+        Text,
+        Sound
+    }
+}
